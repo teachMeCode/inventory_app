dd77e59c3f025049987ac739c6ae47a3fee44a42
teachMeCode <brian.purcell7@gmail.com>
2014-08-11 23:17:51 -0700

    Minor controller update

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,21 +8,13 @@ class ItemsController < ApplicationController
     end
   end
 
-  #  def index
-  #   if current_user == Item.find(params[:id]).user
-  #     @items = current_user.items.all
-  #   else
-  #     redirect_to new_session_path
-  #   end
-  # end
-
   def new
     @item = Item.new
   end
 
   def create
-    @item = Item.new(params.require(:Item).permit(:title, :medium, :theme, :size, :status, :location, :dt_finished))
-    if @item.save
+    @items = Item.new(params.require(:items).permit(:title, :medium, :theme, :size, :status, :location, :dt_finished))
+    if @Item.save
       redirect_to items_path
     else
       render 'new'  
@@ -30,11 +22,16 @@ class ItemsController < ApplicationController
   end
 
   def show
+    if current_user
+      @items = current_user.items.all
+    else
+      redirect_to new_session_path
+    end
   end
 
    def edit
     if current_user && current_user.id == Item.find(params[:id]).id
-      @item = Item.find(params[:id])
+      @items_path = Item.find(params[:id])
     else
       redirect_to welcome_path
     end
